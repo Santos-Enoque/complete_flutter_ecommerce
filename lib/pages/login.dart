befed365bc6b22fdc1f6a93344afc7f1ef7acdc7
Santos Enoque <santosenoque@Santoss-MacBook-Pro.local>
2019-03-17 22:13:01 +0530

    login UI

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,8 +12,13 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
   final GoogleSignIn googleSignIn = new GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+
   SharedPreferences preferences;
   bool loading = false;
   bool isLogedin = false;
@@ -43,7 +48,6 @@ class _LoginState extends State<Login> {
   }
 
   Future handleSignIn() async {
-
     preferences = await SharedPreferences.getInstance();
 
     setState(() {
@@ -78,7 +82,7 @@ class _LoginState extends State<Login> {
         await preferences.setString("id", firebaseUser.uid);
         await preferences.setString("username", firebaseUser.displayName);
         await preferences.setString("photoUrl", firebaseUser.displayName);
-      }else{
+      } else {
         await preferences.setString("id", documents[0]['id']);
         await preferences.setString("username", documents[0]['username']);
         await preferences.setString("photoUrl", documents[0]['photoUrl']);
@@ -97,21 +101,162 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height / 3;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: new Text("Login", style: TextStyle(color: Colors.red.shade900),),
-        elevation: 0.1,
-      ),
       body: Stack(
         children: <Widget>[
+          Image.asset(
+            'images/back.jpg',
+            fit: BoxFit.fill,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+//TODO:: make the logo show
+
+          Container(
+            color: Colors.black.withOpacity(0.8),
+            width: double.infinity,
+            height: double.infinity,
+          ),
+
+          Container(
+              alignment: Alignment.topCenter,
+              child: Image.asset('images/lg.png', width: 280.0, height: 240.0,)),
+
+
           Center(
-            child: FlatButton(
-                color: Colors.red.shade900,
-                onPressed: (){
-              handleSignIn();
-            }, child: Text("Sign in / Sign up with google", style: TextStyle(color: Colors.white),)),
+            child: Padding(
+               padding: const EdgeInsets.only(top: 200.0),
+               child: Center(
+                  child: Form(
+                      key: _formKey,
+                      child: ListView(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.white.withOpacity(0.4),
+                              elevation: 0.0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left:12.0),
+                                child: TextFormField(
+                                  controller: _emailTextController,
+                                  decoration: InputDecoration(
+                                    hintText: "Email",
+                                    icon: Icon(Icons.alternate_email),
+                                  ),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      Pattern pattern =
+                                          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                      RegExp regex = new RegExp(pattern);
+                                      if (!regex.hasMatch(value))
+                                        return 'Please make sure your email address is valid';
+                                      else
+                                        return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.white.withOpacity(0.4),
+                              elevation: 0.0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left:12.0),
+                                child: TextFormField(
+                                  controller: _passwordTextController,
+                                  decoration: InputDecoration(
+                                    hintText: "Password",
+                                    icon: Icon(Icons.lock_outline),
+                                  ),
+                                  validator: (value){
+                                    if(value.isEmpty){
+                                      return "The password field cannot be empty";
+                                    }else if(value.length < 6){
+                                      return "the password has to be at least 6 characters long";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: Colors.blue.shade700,
+                              elevation: 0.0,
+                              child: MaterialButton(onPressed: (){},
+                              minWidth: MediaQuery.of(context).size.width,
+                                child: Text("Login", textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),),
+                              )
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Forgot password", textAlign: TextAlign.center, style: TextStyle(color: Colors.white,  fontWeight: FontWeight.w400,),),
+                          ),
+//                          Expanded(child: Container()),
+
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RichText(text: TextSpan(
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 16.0),
+                              children:[
+                                TextSpan(
+                                  text: "Dont't have an accout? click here to"
+                                ),
+                                TextSpan(
+                                    text: " sign up!",
+                                  style: TextStyle(color: Colors.red)
+
+                                )
+                              ]
+                            ))
+//                            Text("Dont't have an accout? click here to sign up!",textAlign: TextAlign.end, style: TextStyle(color: Colors.white,  fontWeight: FontWeight.w400, fontSize: 16.0),),
+                          ),
+                          Divider(color: Colors.white,),
+                            Text("Other login in opntion",textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: new Material(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Colors.red,
+                                  elevation: 0.0,
+                                  child: MaterialButton(onPressed: (){
+                                    handleSignIn();
+                                  },
+                                    minWidth: MediaQuery.of(context).size.width,
+                                    child: Row(
+                                      children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Image.asset('images/google.png', width: 30.0, height: 30.0,),
+                                    ),
+                                        Text(" google", textAlign: TextAlign.center,
+                                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 22.0),),
+                                      ],
+                                    ),
+                                  )
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+             ),
           ),
 
           Visibility(
@@ -121,7 +266,7 @@ class _LoginState extends State<Login> {
                 alignment: Alignment.center,
                 color: Colors.white.withOpacity(0.9),
                 child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
                 ),
               ),
             ),
@@ -131,3 +276,4 @@ class _LoginState extends State<Login> {
     );
   }
 }
+
